c68c160cdc0fcb359c34ab5c0205b354607ea4a4
Alexander <mail@alexandersaupe.de>
2024-12-13 10:54:06 +0100

    Push js-ele

--- js-createelement/js-createelement_social-media-post/js/index.js
+++ js-createelement/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,21 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+// newPost
+const newPost = document.createElement("article");
+newPost.classList.add("post");
+
+document.body.append(newPost);
+
+// newsPostContent
+const newPostContent = document.createElement("p");
+newPostContent.classList.add("post__content");
+newPostContent.textContent = "test2";
+newPost.append(newPostContent);
+
+// newPostFooter
+const newPostFooter = document.createElement("footer");
+newPostFooter.classList.add("post__footer");
+
+newPost.append(newPostFooter);
