375f8fa96348e09f876feebdb00c6f17f8248ad3
Alexander <mail@alexandersaupe.de>
2024-12-13 17:16:07 +0100

    Add for-each array solution

--- js-array-methods/js-array-methods_for-each/js/index.js
+++ js-array-methods/js-array-methods_for-each/js/index.js
@@ -19,4 +19,20 @@ const colors = [
   "#A5A5A5",
 ];
 
-colors.forEach();
+colors.forEach((colorBox) => {
+  const container = document.createElement("div");
+  container.classList.add("color-container");
+  container.style.backgroundColor = colorBox;
+  container.textContent = colorBox;
+  document.body.append(container);
+});
+
+colors.forEach(renderColorBox);
+
+function renderColorBox(colores) {
+  const container = document.createElement("div");
+  container.classList.add("color-container");
+  container.style.backgroundColor = colores;
+  container.textContent = colores;
+  document.body.append(container);
+}
