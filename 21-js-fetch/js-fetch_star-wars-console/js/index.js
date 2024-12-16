d56d64a0edaded6624f2fec7ac958d16604d2f14
Alexander <mail@alexandersaupe.de>
2024-12-16 17:20:14 +0100

    Add revised solution for 21 star-wars-console

--- 21-js-fetch/js-fetch_star-wars-console/js/index.js
+++ 21-js-fetch/js-fetch_star-wars-console/js/index.js
@@ -1,8 +1,10 @@
 console.clear();
 
+const url = "https://swapi.py4e.com/api/people";
+
 async function fetchData() {
-  const url = await fetch("https://swapi.py4e.com/api/people");
-  const data = await url.json();
+  const response = await fetch(url);
+  const data = await response.json();
   console.log(data);
   console.log(data.results[3]);
   console.log("Eye color of R2-D2:", data.results[2].eye_color);
