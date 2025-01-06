446482c582d5711649f5f1065ddab8ae0800fb55
Alexander <mail@alexandersaupe.de>
2025-01-06 12:25:55 +0100

    Add solution to 30.3/attributes

--- 30-react-basics/react-basics_attributes/src/App.jsx
+++ 30-react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,20 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return Article();
+}
+
+function Article() {
+  return (
+    <article>
+      <h2 className="article_header">Article Header H</h2>
+      <label id="label">Test Article Lorem Ipsum Text</label>
+      <br />
+      <input htmlFor="test"></input>
+      <br />
+      <a className="article_link" href="https://www.wikipedia.org/">
+        Wikipedia
+      </a>
+    </article>
+  );
 }
