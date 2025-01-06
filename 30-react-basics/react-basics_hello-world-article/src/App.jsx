0cc9f016fc83793cff04b02f71c09f3d1ada6481
Alexander <mail@alexandersaupe.de>
2025-01-06 12:07:24 +0100

    Add solution to 30.2/hello-world-article

--- 30-react-basics/react-basics_hello-world-article/src/App.jsx
+++ 30-react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,14 @@
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return HelloWorldArticle();
+}
+
+function HelloWorldArticle() {
+  return (
+    <div>
+      <h1>Article H1</h1>
+      <p>Lorem Ipsum.</p>
+    </div>
+  );
 }
