184dc0103b90631008b38371c8ae4ccdf24fffbd
Alexander <mail@alexandersaupe.de>
2025-01-10 15:11:23 +0100

    Add solution to 38.1-stored-note

--- 38-react-with-local-storage/react-with-local-storage_stored-note/src/App.jsx
+++ 38-react-with-local-storage/react-with-local-storage_stored-note/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+// import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 import FontSelector from "./components/FontSelector";
 import Note from "./components/Note";
 import "./styles.css";
 
 export default function App() {
-  const [note, setNote] = useState("");
-  const [font, setFont] = useState("system-ui");
+  // const [note, setNote] = useState("");
+  // const [font, setFont] = useState("system-ui");
+  const [note, setNote] = useLocalStorageState("note", { defaultValue: 0 });
+  const [font, setFont] = useLocalStorageState("font", {
+    defaultValue: "system-ui",
+  });
 
   function handleNoteChange(newNote) {
     setNote(newNote);
